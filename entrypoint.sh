19c79cbccb9b88e82fd9249f7b32c45a6efe21af
henhaoqi <henhaoqicn@dingtalk.com>
2023-09-09 11:44:05 +0800

    Update 202309091143

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-exec ./alist server --no-prefix
+# 设置您希望的用户和组
+USER=myappuser
+GROUP=10001
+
+chown -R $USER:$GROUP /myapp/alist/
+
+# 设置适当的 umask 值
+umask 0022
+
+# 运行命令
+./alist server --no-prefix
