d035d650c61729d6a086ecb7cd3ef530335c5a8d
henhaoqi <henhaoqicn@dingtalk.com>
2023-09-11 10:35:13 +0800

    Delete unuse files

--- entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-chown -R ${PUID}:${PGID} /opt/alist/
-
-umask ${UMASK}
-
-exec su-exec ${PUID}:${PGID} ./alist server --no-prefix
-
-exec su-exec ${PUID}:${PGID} ./alist admin set henhaoqi-github
